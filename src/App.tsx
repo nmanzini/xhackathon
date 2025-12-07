42b3a017e6fe877e23c715bc6a42073f87de8dd6
sampsoon <28665112+Sampsoon@users.noreply.github.com>
2025-12-06 22:18:20 -0800

    Some change idk where this came from

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { CodeEditor, InterviewPanel } from './components'
+import { useState } from "react";
+import { CodeEditor, InterviewPanel } from "./components";
 
 const defaultCode = `// Welcome to the Voice AI Interview
 // Write your solution below
@@ -7,24 +7,21 @@ const defaultCode = `// Welcome to the Voice AI Interview
 function solution() {
   // Your code here
 }
-`
+`;
 
 function App() {
-  const [code, setCode] = useState(defaultCode)
+  const [code, setCode] = useState(defaultCode);
 
   return (
     <div className="h-screen w-screen flex bg-zinc-950">
       <div className="flex-1 h-full">
-        <CodeEditor 
-          value={code} 
-          onChange={(value) => setCode(value ?? '')} 
-        />
+        <CodeEditor value={code} onChange={(value) => setCode(value ?? "")} />
       </div>
       <div className="w-80 h-full border-l border-zinc-700">
         <InterviewPanel />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
